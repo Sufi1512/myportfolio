be0a44f62f200a7bdc4f75f079ae5bfd3d143e74
Sufi1512 <ksufiyan38@gmai.com>
2023-11-14 12:22:39 +0530

    Added: About & TechStack

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Layout from "./components/layout/layout";
+import About from "./pages/About/About";
+import TechStack from "./pages/TechStack/TechStack";
 
 
 function App() {
   return (
     <>
-   <Layout/>  
+      <Layout />
+      <About />
+      <TechStack />
     </>
   );
 }

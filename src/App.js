a2a5fae17eaaebc29613d180b906300ab18f947a
Sufi1512 <ksufiyan38@gmai.com>
2023-11-20 00:51:35 +0530

    Added: Experience and contacts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Layout from "./components/layout/layout";
 import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Education from "./pages/Education/Education";
 import Projects from "./pages/Projects/Projects";
 import TechStack from "./pages/TechStack/TechStack";
+import WorkExp from "./pages/WorkExp/WorkExp";
 
 
 function App() {
   return (
     <>
       <Layout />
+     
       <About />
+      <Education />
       <TechStack />
       <Projects />
+      <WorkExp />
+      <Contact />
     </>
   );
 }

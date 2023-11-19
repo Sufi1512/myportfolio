968f4ef0b2f28014bccc86505c6a06851d4324db
Sufi1512 <ksufiyan38@gmai.com>
2023-11-20 01:07:06 +0530

    Add Work Experience section

---
+++ src/pages/WorkExp/WorkExp.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./WorkExp.css";
+
+const WorkExp = () => {
+  return (
+    <>
+      <div className="work">
+        <div className="workexp">
+          <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">
+            Work Experience Update  Soon
+          </h2>
+          <hr />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WorkExp;
